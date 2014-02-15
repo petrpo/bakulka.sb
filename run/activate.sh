2db59af3d1f592f08b94872b27938c5727990523
Petr Podrabsky <petrpo@gmail.com>
2014-02-15 20:42:05 +0100

    CHANGE: light modification of script

--- run/activate.sh
+++ run/activate.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # This script sets neccesary apps for run bakulka
+# 2014-02-14 first init
 
 echo "Hello World. Let's have a fun !!"
+echo
 
 # we can do activation of apps in future
 #ACTIVE_APPS_FILE=/etc/active_apps
@@ -14,7 +16,7 @@ echo "Hello World. Let's have a fun !!"
 
 function set_httpd {
     echo "Settings in file httpd.conf "
-    # uncomment line
+    # allow mod_php
     sed -i 's/\#Include\ \/etc\/httpd\/mod_php.conf/Include\ \/etc\/httpd\/mod_php.conf/g' /etc/httpd/httpd.conf
 #    echo "AddHandler application/x-httpd-php .php .html -html" >> /etc/httpd/httpd.conf
     sed -i 's/\#ServerName\ www.example.com:80/ServerName\ 127.0.0.1/' /etc/httpd/httpd.conf
@@ -22,13 +24,13 @@ function set_httpd {
 
 function set_mysql {
     echo "Setting mysql rights"
-    mysql_install_db
+    mysql_install_db > /dev/null
     chown -R mysql:mysql /var/lib/mysql
 }
 
-# Setting in httpd 
+# Settings of apps
 set_httpd
-echo "https is set"
+echo "httpd is set"
 
 set_mysql
 echo "mysql is set"
@@ -39,3 +41,5 @@ echo "httpd is running"
 
 /usr/share/mysql/mysql.server start
 echo "mysql is running"
+
+echo "There's nothing more to say"
