5f9a0d8e71cc9ab601d8d77e1ac9996fee1ab975
Petr Podrabsky <petrpo@gmail.com>
2014-02-15 20:22:27 +0100

    CHANGE: in script added setting of httpd

--- run/activate.sh
+++ run/activate.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# This script sets neccesary apps for bakulka
+# This script sets neccesary apps for run bakulka
 
-echo "Hello World. Here we are !!"
+echo "Hello World. Let's have a fun !!"
 
 # we can do activation of apps in future
 #ACTIVE_APPS_FILE=/etc/active_apps
@@ -16,12 +16,17 @@ function set_httpd {
     echo "Settings in file httpd.conf "
     # uncomment line
     sed -i 's/\#Include\ \/etc\/httpd\/mod_php.conf/Include\ \/etc\/httpd\/mod_php.conf/g' /etc/httpd/httpd.conf
-    echo "AddHandler application/x-httpd-php .php .html -html" >> /etc/httpd/httpd.conf
-    echo "ServerName 127.0.0.1" >> /etc/httpd/httpd.conf
+#    echo "AddHandler application/x-httpd-php .php .html -html" >> /etc/httpd/httpd.conf
+    sed -i 's/\#ServerName\ www.example.com:80/ServerName\ 127.0.0.1/' /etc/httpd/httpd.conf
 }
 
 function set_mysql {
     echo "Setting mysql rights"
     # chown -R mysql:mysql /var/lib/mysql
 }
+
+# Setting in httpd 
+set_httpd
+echo "https is set"
+
 
