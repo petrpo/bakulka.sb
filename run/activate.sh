094c3e70d36b472d310e9cfa6377bcc2148045ed
Petr Podrabsky <petrpo@gmail.com>
2014-02-15 22:14:43 +0100

    ADD: DirectoryIndex index.php index.html

--- run/activate.sh
+++ run/activate.sh
@@ -23,6 +23,7 @@ function set_httpd {
     sed -i 's/\#ServerName\ www.example.com:80/ServerName\ 127.0.0.1/' /etc/httpd/httpd.conf
     sed -i 's/Options\ Indexes\ FollowSymLinks/Options\ -Indexes\ +FollowSymLinks/' /etc/httpd/httpd.conf
     echo "AddHandler application/x-httpd-php .php .html -html" >> /etc/httpd/httpd.conf
+    echo "DirectoryIndex index.php index.html" >> /etc/httpd/httpd.conf
 }
 
 function set_mysql {
