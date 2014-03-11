9e6095fb936047f531511e48327029ff706d842f
Petr Podrabsky <petrpo@gmail.com>
2014-03-11 19:25:17 +0100

    CHANGE: add menus in index.html

---
+++ var/www/htdocs/php/index.php
@@ -0,0 +1,19 @@
+<?php
+
+echo "<header></header>";
+echo "<title></title>";
+echo "<body></body>";
+
+include menu.php;
+
+# ip address, gw, APN, slip
+
+<p>your APN : <input type="text" name="psc" size="10" maxlenght="6" /></p>
+
+
+<form>
+
+</form>
+
+
+?>
